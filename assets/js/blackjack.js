0f0995d8579de0e3568c1ae055d95f414fbeb75d
vmorenomarin <vmorenomarin@gmail.com>
2021-08-23 22:23:42 -0500

    Se escribe el código para el funcionamiento de los botores detener, juego nuevio: también se escriben las funciones para el turno de la máquina y los avisos de derrota o victoria dependiendo de los puntajes obtenidos en los turnos del jugador y la máquina.

--- assets/js/blackjack.js
+++ assets/js/blackjack.js
@@ -3,18 +3,20 @@
 
 let deck = [];
 const letters = ['C', 'D', 'H', 'S'];
-const mayors = ['A','J','Q','K']
+cons mayors = ['A','J','Q','K']
 
 // Otras variables
 
-let playerPoints = 0, machinePoints = 0;
+let playerScore = 0, machineScore = 0;
 
-// Las siguientes líneas crean variables que hacen referencias a los elementos del HTML.
+// Las siguientes líneas crean variables que hacen referencias a los elementos del DOM.
 
 const btnQueryCard = document.querySelector('#btnQueryCard');
-console.log(btnQueryCard);
-const smallPoints = document.querySelectorAll('small');
-const divplayerCards = document.querySelector('#player-cards')
+const btnStop = document.querySelector('#btnStop')
+const btnNewGame = document.querySelector('#btnNewGame')
+const smallScore = document.querySelectorAll('small');
+const divPlayerCards = document.querySelector('#player-cards')
+const divMachineCards = document.querySelector('#machine-cards')
 
 const deckGenerator = () => {
     for ( let number = 2; number <= 10;number++ ) {
@@ -28,49 +30,103 @@ const deckGenerator = () => {
             deck.push(mayor + letter)
         }
     }
-    deck = _.shuffle(deck); // La función shufle se importa desde la librería underscore. Permite desordenar el mazo.
-    console.log(deck);
+    deck = _.shuffle(deck); // La función shuffle se importa desde la librería underscore. Permite desordenar el mazo.
     return deck;
 }
 
-// Las siguientes líneas me permiten extraer una carta aleatoria del mazo. Esta función se usará dentro de la característica "Pedir carta"
+// Las siguientes líneas me permiten extraer una carta aleatoria del mazo. Esta función se usará dentro de la característica "Pedir carta".
 const requestCard = () => {
     if (deck.length >= 1) {
         let card = deck.pop();
         return card;
     }
     else {
-        throw "No hay cartas en el mazo."
+        throw "No hay cartas en el mazo." // throw me permite mostrar en consola mensaje de excepción.
     }
 }
 
 // Las siguientes líneas me permitirán asignar un valor a la carta que retorna la función requestCard.
 const cardValue = ( card ) => {
-    let value = card.substring( 0, card.length-1);
+    let value = card.substring( 0, card.length-1);  // El método substring usado sobre el string, me permite tomar determinado rango de caracteres dentro del rango completo de la cadena que es un string. 
     return isNaN(value) ? 
         (value === 'A' ? 11 : 10)
         : value * 1
+} // En las anteriores líneas, después del return, se hizo uso del operador ternario para evaluar un conjunto de condiciones en pocas líneas.\
+
+// Turno computadora.
+machineTurn = ( minScore ) => {
+    do {
+        const card = requestCard();
+        machineScore = machineScore + cardValue(card);
+        smallScore[1].innerText = machineScore;
+
+        const cardImage = document.createElement('img'); // Estas líneas permiten mostrar las imágenes de las cartas cada vez que se pida una carta.
+        cardImage.src =`/assets/cards/${card}.png`;
+        cardImage.classList.add('carta');
+        divMachineCards.append(cardImage);
+
+        if(minScore > 21) {
+            break;
+        }
+    } while( (machineScore < minScore) && (minScore < 21));
+
+    setTimeout( () => {
+    if (machineScore === minScore) {
+        alert("Nadie ha ganado.");
+    } else if (minScore > 21) {
+        alert("La máquina ha ganado.");
+    } else if (machineScore > 21) {
+        alert("¡Has ganado!");
+    } else {
+        alert("Computadora gana.")
+    }},100);
 }
 
-// Las siguientes cuentan generan los eventos de los botones.
+// Las siguientes líneas generan los eventos de los botones asociados al turno del jugador.
 deckGenerator();
 btnQueryCard.addEventListener('click', () => {
     const card = requestCard();
-    playerPoints = playerPoints + cardValue(card);
-    smallPoints[0].innerText = playerPoints;
+    playerScore = playerScore + cardValue(card);
+    smallScore[0].innerText = playerScore;
 
-    const cardImage = document.createElement('img');
+    const cardImage = document.createElement('img'); // Estas líneas permiten mostrar las imágenes de las cartas cada vez que se pida una carta.
     cardImage.src =`/assets/cards/${card}.png`;
     cardImage.classList.add('carta');
-    divplayerCards.append(cardImage);
+    divPlayerCards.append(cardImage);
 
-    if (playerPoints > 21) {
-        console.warn("Has perdido.");
+    if (playerScore > 21) {
+        btnStop.disabled = true;
         btnQueryCard.disabled = true;
-    } else if (playerPoints === 21) {
-        console.warn("21, has ganado.");
+        machineTurn(playerScore);
+    } else if (playerScore === 21) {
+        alert("¡Has ganado!");
+        btnStop.disabled = true;
+        btnQueryCard.disabled = true;
+        machineTurn(playerScore);
     }
-
 })
 
-/* <img class="carta" src="/assets/cards/10C.png" alt="" ></img> */
+btnStop.addEventListener('click', () =>{
+    btnStop.disabled = true;
+    btnQueryCard.disabled = true;
+    machineTurn(playerScore);
+})
+
+btnNewGame.addEventListener('click', () => {
+    console.clear();
+    
+    deck = []
+    deck = deckGenerator();
+    playerScore = 0, machineScore = 0;
+    
+    divPlayerCards.innerHTML = " ";
+    divMachineCards.innerHTML = " ";
+
+    smallScore[0].innerHTML = " ";
+    smallScore[1].innerHTML = " ";
+
+    btnQueryCard.disabled = false;
+    btnStop.disabled = false;
+    
+}
+)
